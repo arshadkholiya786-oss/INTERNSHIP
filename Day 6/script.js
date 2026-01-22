de1197125249facc61bfb35d422a336134fe5222
arshadkholiya786-oss <arshadkholiya786@gmail.com>
2026-01-22 16:39:06 +0530

    Add files via upload

---
+++ Day 6/script.js
@@ -0,0 +1,7 @@
+document.querySelector(".email-box button").addEventListener("click", () => {
+  alert("Thanks for subscribing!");
+});
+
+document.querySelector(".newsletter-box button").addEventListener("click", () => {
+  alert("Newsletter subscription successful!");
+});
